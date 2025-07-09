beb2d47a1f6d69e1129016788d7b1da08f505a4b
JeffToken31 <jeffrey-31@hotmail.fr>
2025-07-09 13:17:35 +0200

    update education

--- src/components/Education.js
+++ src/components/Education.js
@@ -10,10 +10,11 @@ function Education() {
     <section id="education" ref={ref} className={`fade-in ${inView ? 'appear' : ''}`}>
       <h2>Formations</h2>
       <ul>
-        <li>Holberton School (Session Mars 2025) : Développement logiciel avec exploration de spécialisations</li>
+        <li>Holberton School: Développement logiciel avec exploration de spécialisations | 2025 | RNCP5</li>
+        <li>IBM Skillsbuild Certification : OpenSourceSoftware, Agile, Data-Base relational SQL | 2025</li>
         <li>FreeCodeCamp.org : Responsive Web Design, JavaScript Algorithms and Data Structures</li>
-        <li>OpenClassrooms.com : Apprenez les bases du langage Python, Créez votre site web avec HTML5 et CSS3</li>
-        <li>BTS Management des Unités Commerciales | 2010</li>
+        <li>OpenClassrooms.com : Apprenez les bases du langage Python, Créez votre site web avec HTML5 et CSS3, site statique et dynamique (PHP)</li>
+        <li>BTS Management des Unités Commerciales | 2010 | RNCP 5</li>
         <li>BAC Pro Commerce | 2008</li>
         <li>BEP Vente Actions Marchandes | 2006</li>
         <li>BEP Hôtellerie Option Cuisine | 2005</li>
