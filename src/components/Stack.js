02057e821e97c4517f9f7d57300d21d28e1e1ad5
JeffToken31 <jeffrey-31@hotmail.fr>
2025-07-10 16:49:41 +0200

    other try

--- src/components/Stack.js
+++ src/components/Stack.js
@@ -1,43 +1,29 @@
 import React from 'react';
 import { useInView } from 'react-intersection-observer';
-import git from './teck_stack/git.png';
-import bash from './teck_stack/bash.png';
-import ubuntu from './teck_stack/ubuntu.png';
-import c from './teck_stack/c.png';
-import css from './teck_stack/css.png';
-import flask from './teck_stack/flask.png';
-import github from './teck_stack/github.png';
-import html from './teck_stack/html.png';
-import linux from './teck_stack/icons8-linux-96.png';
-import js from './teck_stack/js.png';
-import mysql from './teck_stack/mysql.png';
-import react from './teck_stack/react.png';
-import solidity from './teck_stack/solidity.png';
-import python from './teck_stack/python.png';
 
 function Stack() {
     const { ref, inView } = useInView({
-        triggerOnce: true, // Lance l'animation uniquement lors de la première vue
-        threshold: 0.2, // L'animation démarre lorsque 10% de la section sont visibles
+        triggerOnce: true,
+        threshold: 0.2,
     });
     return (
         <section id="Stack" ref={ref} className={`fade-in ${inView ? 'appear' : ''}`}>
             <div id="logos_box">
-            <img src={git} alt="git" className="stack" />
-            <img src={github} alt="github" className="stack" />
-            <img src={linux} alt="linux" className="stack" />
-            <img src={ubuntu} alt="ubuntu" className="stack" />
-            <img src={bash} alt="bash" className="stack" />
-            <img src={c} alt="c" className="stack" />
-            <img src={solidity} alt="solidity" className="stack" />
-            <img src={python} alt="python" className="stack" />
-            <img src={flask} alt="flask" className="stack" />
-            <img src={mysql} alt="mysql" className="stack" />
-            <img src={js} alt="js" className="stack" />
-            <img src={react} alt="react" className="stack" />
-            <img src={html} alt="html" className="stack" />
-            <img src={css} alt="css" className="stack" />
-           </div>
+                <img src="/teck_stack/git.png" alt="git" className="stack" />
+                <img src="/teck_stack/github.png" alt="github" className="stack" />
+                <img src="/teck_stack/icons8-linux-96.png" alt="linux" className="stack" />
+                <img src="/teck_stack/ubuntu.png" alt="ubuntu" className="stack" />
+                <img src="/teck_stack/bash.png" alt="bash" className="stack" />
+                <img src="/teck_stack/c.png" alt="c" className="stack" />
+                <img src="/teck_stack/solidity.png" alt="solidity" className="stack" />
+                <img src="/teck_stack/python.png" alt="python" className="stack" />
+                <img src="/teck_stack/flask.png" alt="flask" className="stack" />
+                <img src="/teck_stack/mysql.png" alt="mysql" className="stack" />
+                <img src="/teck_stack/js.png" alt="js" className="stack" />
+                <img src="/teck_stack/react.png" alt="react" className="stack" />
+                <img src="/teck_stack/html.png" alt="html" className="stack" />
+                <img src="/teck_stack/css.png" alt="css" className="stack" />
+            </div>
         </section>
     )
 }
